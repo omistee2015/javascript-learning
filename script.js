0f955a45bf888efd9bc6cbd87aeada2332e684f3
Tope Omirefa <omistee@gmail.com>
2026-09-10 22:47:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,36 @@ let customers = [];
 function addCustomer() {
 
     let name = document.getElementById("customerName").value;
-
     let service = document.getElementById("customerService").value;
-
     let price = document.getElementById("customerPrice").value;
 
-    console.log(name);
-    console.log(service);
-    console.log(price);
+    let customer = {
+        name: name,
+        service: service,
+        price: price
+    };
+
+    customers.push(customer);
+
+    displayCustomers();
+
+}
+
+function displayCustomers() {
+
+    let customerList = document.getElementById("customerList");
+
+    customerList.innerHTML = "";
+
+    for (let i = 0; i < customers.length; i++) {
+
+        customerList.innerHTML +=
+            "<div>" +
+            "<h3>" + customers[i].name + "</h3>" +
+            "<p>Service: " + customers[i].service + "</p>" +
+            "<p>Price: $" + customers[i].price + "</p>" +
+            "</div>";
+
+    }
 
 }
