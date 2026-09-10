9c09387e540a51d40c70dd8f23425049368c4c11
Tope Omirefa <omistee@gmail.com>
2026-09-10 11:44:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-function showNumbers() {
+let customers = [
+    {
+        name: "John",
+        service: "CCTV Installation",
+        price: 150
+    },
 
-    for (let i = 1; i <= 5; i++) {
+    {
+        name: "David",
+        service: "Solar Inverter",
+        price: 500
+    },
 
-        console.log(i);
-
+    {
+        name: "Michael",
+        service: "Web Development",
+        price: 300
     }
+];
+
+for (let i = 0; i < customers.length; i++) {
+
+    console.log(customers[i].name);
+    console.log(customers[i].service);
+    console.log(customers[i].price);
 
 }
